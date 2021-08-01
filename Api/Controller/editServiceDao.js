b7b2675eb180036c3225027d9610192bd4245f72
Abinash Biswal <abinashbiswal248@gmail.com>
2021-08-01 21:06:30 +0530

    UPDATE-01: Added functions to update profile picture and credentials

--- Api/Controller/editServiceDao.js
+++ Api/Controller/editServiceDao.js
@@ -21,3 +21,39 @@ module.exports.saveEditedData = async function (email, fields, data) {
     throw exc;
   }
 };
+
+/**
+ * @function updateProfilePicture
+ * @async
+ * @description update user's profile picture
+ * @returns Boolean (True/False)
+ */
+module.exports.updateProfilePicture = async function (email, blob) {
+  try {
+    var imageSaved = await db.updateMedia("email", email, blob);
+    return imageSaved;
+  } catch (exc) {
+    throw exc;
+  }
+};
+
+/**
+ * @function updateProfilePicture
+ * @async
+ * @description update user's credential
+ * @returns Boolean (True/False)
+ */
+module.exports.updateCredential = async function (email, cred) {
+  try {
+    var credentialSaved = await db.update(
+      DATABASE.CREDENTIALS,
+      "email",
+      email,
+      ["password"],
+      [cred]
+    );
+    return credentialSaved;
+  } catch (exc) {
+    throw exc;
+  }
+};
