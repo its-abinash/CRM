b2c1a07521cc4804b98a0069e79f8b67e27918f2
Abinash Biswal <abinashbiswal248@gmail.com>
2021-09-10 17:57:46 +0530

    ENH-13: Added minor changes

--- Api/Controller/initTasks.js
+++ Api/Controller/initTasks.js
@@ -1,10 +1,10 @@
-const rmqConsumer = require("../Broker/rmq.consumer");
+const rmqConsumer = require("../Broker/consumerUtils");
 const logger = require("../Logger/log");
 
 module.exports.run_init_job = async function () {
   try {
     logger.info("Setting up RabbitMQ Server");
-    rmqConsumer.consume();
+    await rmqConsumer.consumeUtil()
     logger.info("RabbitMQ Server is Up & Running...");
   } catch (ex) {
     logger.error(`Error while connecting to RabbitMQ Server: ${ex}`);
